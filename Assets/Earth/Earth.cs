618409e6aafb2746f6fe7c0a7c84358cdb8c1272
Alimen <alimen@gmail.com>
2015-10-31 19:39:40 +0800

    Add earth roll-out animations.

--- Assets/Earth/Earth.cs
+++ Assets/Earth/Earth.cs
@@ -5,8 +5,15 @@ public class Earth : MonoBehaviour
 {
 	// Component handlers
 	public Spining spining;
-	public EarthWind[] earthWinds;
+	public Animator anim;
 	public StartHint startHint;
+	public EarthWind[] earthWinds;
+
+	// Animation control
+	public float skyboxAlpha;
+	public Skybox skybox;
+	public Color cameraColorControl;
+	public Camera mainCamera;
 
 	// Rotation parameters
 	bool mouseover = false;
@@ -15,11 +22,46 @@ public class Earth : MonoBehaviour
 	public float accel;
 	public float pacificMiddleLine;
 	public float pacificWidth;
+	public float animStartLine;
 
-	void Update()
+	// State parameters
+	public bool clickAvaialble;
+	public enum state { idle, transiting, animating, stopped };
+	public state currentState;
+
+	void Start()
+	{
+		currentState = state.idle;
+		StartCoroutine(update());
+	}
+
+	IEnumerator update()
+	{
+		yield return StartCoroutine(idle());
+		yield return StartCoroutine(transit());
+		yield return StartCoroutine(rolloutAnimation());
+	}
+
+	///////////////////////////////////////////////////////////////////////////////
+	//
+	// Idle
+	//
+	///////////////////////////////////////////////////////////////////////////////
+
+	IEnumerator idle()
+	{
+		while (currentState == state.idle) {
+			idleStep();
+			yield return null;
+		}
+	}
+
+	void idleStep()
 	{
 		Vector3 speed = spining.speed;
-		if (mouseover && !(Mathf.Abs(Mathf.DeltaAngle(transform.localEulerAngles.y, pacificMiddleLine)) < pacificWidth)) {
+		clickAvaialble = (Mathf.Abs(Mathf.DeltaAngle(transform.localEulerAngles.y, pacificMiddleLine)) < pacificWidth);
+
+		if (mouseover && !clickAvaialble) {
 			if (speed.z > topSpeed) {
 				speed.z += accel * Time.deltaTime;
 				if (speed.z < topSpeed) {
@@ -36,25 +78,94 @@ public class Earth : MonoBehaviour
 		}
 		spining.speed = speed; 
 	}
+	
+	///////////////////////////////////////////////////////////////////////////////
+	//
+	// Transit
+	//
+	///////////////////////////////////////////////////////////////////////////////
+
+	IEnumerator transit()
+	{
+		while (Mathf.Abs(Mathf.DeltaAngle(transform.localEulerAngles.y, animStartLine)) > Mathf.Abs(topSpeed * Time.deltaTime)) {
+			transitStep();
+			yield return null;
+		}
+
+		anim.enabled = true;
+		foreach (EarthWind e in earthWinds) {
+			e.gameObject.SetActive(false);
+		}
+		currentState = state.animating;
+	}
+
+	void transitStep()
+	{
+		Vector3 speed = spining.speed;
+		if (speed.z > topSpeed) {
+			speed.z += accel * Time.deltaTime;
+			if (speed.z < topSpeed) {
+				speed.z = topSpeed;
+			}
+		}
+		spining.speed = speed; 
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////
+	//
+	// Roll-out animation
+	//
+	///////////////////////////////////////////////////////////////////////////////
+
+	IEnumerator rolloutAnimation()
+	{
+		while (currentState == state.animating) {
+			skybox.alpha = skyboxAlpha;
+			mainCamera.backgroundColor = cameraColorControl;
+			yield return null;
+		}
+	}
+
+	public void animationEnds()
+	{
+		currentState = state.stopped;
+		Destroy(gameObject);
+		Destroy(skybox.gameObject);
+	}
+
+	///////////////////////////////////////////////////////////////////////////////
+	//
+	// Mouse events
+	//
+	///////////////////////////////////////////////////////////////////////////////
 
 	void OnMouseEnter()
 	{
-		mouseover = true;
-		foreach (EarthWind e in earthWinds) {
-			e.on = true;
+		if (currentState == state.idle) {
+			mouseover = true;
+			foreach (EarthWind e in earthWinds) {
+				e.on = true;
+			}
 		}
 	}
 
 	void OnMouseExit()
 	{
-		mouseover = false;
-		foreach (EarthWind e in earthWinds) {
-			e.on = false;
+		if (currentState == state.idle) {
+			mouseover = false;
+			foreach (EarthWind e in earthWinds) {
+				e.on = false;
+			}
 		}
 	}
 
 	void OnMouseUp()
 	{
-		startHint.on = false;
+		if (clickAvaialble && currentState == state.idle) {
+			OnMouseExit();
+			startHint.on = false;
+			clickAvaialble = false;
+			currentState = state.transiting;
+		}
 	}
 }
---
+++ Assets/Skybox/Skybox.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skybox : MonoBehaviour
+{
+	public Renderer rend;
+
+	public float alpha {
+		set {
+			Color c;
+			foreach (Material m in rend.materials) {
+				c = m.color;
+				c.a = Mathf.Clamp01(value);
+				m.color = c;
+			}
+		}
+	}
+}
