1f5530f0aa34f0eddcb8b63f688885b5a68e325f
Alimen <alimen@gmail.com>
2016-05-10 21:50:21 +0800

    Add Stopwatch.js

---
+++ Stopwatch.js
@@ -0,0 +1,15 @@
+var startTime = new Date().getTime();
+var formKey = "1vfIIx0uu51rNb6S9TR2openuR8LXon3NdRiK3bBgiSQ";
+var entryID = "495906984";
+
+function stopTimer() {
+	return t = (new Date().getTime() - startTime) / 1000;
+}
+
+var sendToGoogle = function() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "https://docs.google.com/forms/d/" + formKey + "/formResponse?entry." + entryID + "=" + stopTimer() + "&submit=Submit", true);
+	xhttp.send();
+}
+
+window.onbeforeunload = sendToGoogle;
